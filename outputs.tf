4f30a8c2a52e03e7307e409abfcb57acaf292ba3
Ukie555 <309538567@qq.com>
2024-11-05 10:33:44 -0800

    Refactor to use variables

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "instance_ami" {
- value = aws_instance.blog.instance_ami
-}
+# output "instance_ami" {
+#  value = aws_instance.blog.instance_ami
+# }
 
-output "instance_arn" {
- value = aws_instance.blog.instance_arn
-}
+# output "instance_arn" {
+#  value = aws_instance.blog.instance_arn
+# }
